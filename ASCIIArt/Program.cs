e98b8643a44c464f3ceb445aeed411160e0604d0
epikkoder <ronaldsahagun87@gmail.com>
2021-11-13 21:40:11 -0800

    Add project

---
+++ ASCIIArt/Program.cs
@@ -0,0 +1,17 @@
+﻿using ImageMagick;
+using System;
+
+namespace ASCIIArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var image = new MagickImage(@"C:\Repos\ASCIIArt\ASCIIArt\ascii-pineapple.jpg"))
+            {
+                Console.WriteLine("Loaded image!");
+                Console.WriteLine($"Image size: {image.Width} x {image.Height}");
+            }
+        }
+    }
+}
