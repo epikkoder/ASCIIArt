7d770dcf2ad461e897a6dfd79cc1a66df79f115d
Ronald Sahagun <ronaldsahagun87@gmail.com>
2021-12-28 10:20:59 -0800

    Refactor pixel array variable & remove test code

--- ASCIIArt/Program.cs
+++ ASCIIArt/Program.cs
@@ -9,7 +9,7 @@ namespace ASCIIArt
         static void Main(string[] args)
         {
             var image = new MagickImage(@"C:\Repos\ASCIIArt\ASCIIArt\ascii-pineapple.jpg");
-            var pixelArray = new List<int[]>();
+            var pixelArrays = new List<int[]>();
 
             using (var pixels = image.GetPixels())
             {
@@ -18,19 +18,16 @@ namespace ASCIIArt
                     for (int y = 0; y < image.Height; y++)
                     {
                         var pixelValue = pixels.GetValue(x, y);
-                        pixelArray.Add(new int[] { pixelValue[0], pixelValue[1], pixelValue[2] });
+                        pixelArrays.Add(new int[] { pixelValue[0], pixelValue[1], pixelValue[2] });
                     }
                 }
             }
-
-            var test = pixelArray[0];
-            test[test.Length] = 4;
 
             Console.WriteLine("Loaded image!");
             Console.WriteLine($"Image size: {image.Width} x {image.Height}");
             Console.WriteLine("Iterating through pixel contents:");
 
-            foreach (var array in pixelArray)
+            foreach (var array in pixelArrays)
             {
                 Console.WriteLine($"({array[0]}, {array[1]}, {array[2]})");
             }
